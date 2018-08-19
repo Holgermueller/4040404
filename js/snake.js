3a8191077b6460d4ba3714d363ff77fa5162a2af
Holger Mueller <9jholger@gmail.com>
2018-08-19 13:00:22 -0500

    got gameboard on dom

--- js/snake.js
+++ js/snake.js
@@ -3,5 +3,33 @@ const ctx = cvs.getContext('2d');
 
 const box = 32;
 
+// get images
 const ground = new Image();
 ground.src = './img/ground.png';
+
+const foodImg = new Image();
+foodImg.src = './img/food.png';
+
+// create the snake
+let snake = [0];
+snake[0] = {
+    x: 9 * box,
+    y: 10 * box,
+};
+
+// create apples
+let apple = {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box,
+}
+
+// create score
+let score = 0;
+
+// draw everything on canvas
+function draw() {
+    ctx.drawImage(ground, 0, 0);
+}
+
+// call draw function every 100ms
+let game = setInterval(draw, 100)
